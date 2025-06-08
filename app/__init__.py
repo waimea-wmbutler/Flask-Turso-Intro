fd75f4802d72ace031e6b49b399a9610e7f8c2ac
waimea-wmbutler <wmbutler@waimea.school.nz>
2025-06-09 10:58:50 +1200

    Fixes all error

--- app/__init__.py
+++ app/__init__.py
@@ -6,10 +6,6 @@ from dotenv         import load_dotenv
 import os
 
 
-# Load Turso environment variables from the .env file
-load_dotenv()
-TURSO_URL = os.getenv https://things-waimea-wmbutler.aws-ap-northeast-1.turso.io
-TURSO_KEY = os.getenv eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3NDg1NzkyMzcsImlkIjoiMTg3MzU5ZDktNTgwYy00MTA1LTliNmItMjZiNzMyMzQwMTk3IiwicmlkIjoiOTc1NjY4MzUtMWJiMS00OTBkLWI1NmUtYzcwNjg3Zjc5NThkIn0.H2dVQXhnzkXXR8t6EPm3t3UVn-edwVFiGJ1gPxTtbaFkhhxI-cVq2LGO9hT0lIhPpocefW13fv3-7RAFpw7dCw
 
 # Create the Flask app
 app = Flask(__name__)
@@ -34,10 +30,10 @@ def connect_db():
 @app.get("/")
 def home():
     client = connect_db()
-    result = client.execute("SELECT * FROM things")
-    print(result.rows)
+    result = client.execute("SELECT id, name FROM things")
+    things = result.roles
 
-    return render_template("pages/home.jinja")
+    return render_template("pages/home.jinja", things=things)
 
 
 #-----------------------------------------------------------
@@ -45,7 +41,19 @@ def home():
 #-----------------------------------------------------------
 @app.get("/thing/<int:id>")
 def show_thing(id):
-    return render_template("pages/thing.jinja")
+    client = connect_db()
+    sql="""
+            SELECT 
+            id, 
+            name, 
+            price 
+            FROM things
+            WHERE id=?
+            """
+    values = [id]
+    result = client.execute(sql, values)
+    things = result.roles[0]
+    return render_template("pages/thing.jinja", thing=thing)
 
 
 #-----------------------------------------------------------
